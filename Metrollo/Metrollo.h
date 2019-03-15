ef7f2eeb39e87f4b4cc3a4e3c608ea0bae83ecaf
Kim de Vos <kimdevos12@hotmail.com>
2019-03-15 17:47:15 +0000

    Added base project files

---
+++ Metrollo/Metrollo.h
@@ -0,0 +1,19 @@
+//
+//  Metrollo.h
+//  Metrollo
+//
+//  Created by Kim de Vos on 15/03/2019.
+//  Copyright © 2019 Kim de Vos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Metrollo.
+FOUNDATION_EXPORT double MetrolloVersionNumber;
+
+//! Project version string for Metrollo.
+FOUNDATION_EXPORT const unsigned char MetrolloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Metrollo/PublicHeader.h>
+
+
